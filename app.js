9986cead38351a8fe54ce0bbbc9e3772173885a6
deepak-negi-web <59686852+deepak-negi-web@users.noreply.github.com>
2020-03-13 20:18:50 +0530

    Updated

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.post("/failure", function(req2, res2) {
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("server is running in port 3000....");
 });
